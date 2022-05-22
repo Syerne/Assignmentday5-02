cc37b5d3f1153163e7e925a133b5a3721b915f62
Syerne <shubhamyerne1994@gmail.com>
2022-05-22 21:09:42 +0530

    Added: flip coin program

--- task.sh
+++ task.sh
@@ -1,2 +1,16 @@
 read -p "enter assignment day " day
 
+
+flip=$(( RANDOM%3 ))
+#tail=0
+#head=1
+if(( flip == 0 ))
+then
+ echo "Tails"
+elif(( flip == 1 ))
+then
+ echo "Heads"
+else
+ echo "match tie; again toss"
+fi
+
