6dbbbc7915f071c4a38c4c435c812e350c13bfae
Syerne <shubhamyerne1994@gmail.com>
2022-05-22 21:14:27 +0530

    Added: single digit print in word

--- task.sh
+++ task.sh
@@ -14,3 +14,40 @@ else
  echo "match tie; again toss"
 fi
 
+
+read -p "enter single digit number: " num
+
+if(( num == 0 ))
+then
+    echo "ZERO"
+elif(( num == 1 ))
+then
+    echo "ONE"
+elif(( num == 2 ))
+then
+    echo "TWO"
+elif(( num == 3 ))
+then
+    echo "THREE"
+elif(( num == 4 ))
+then
+    echo "FOUR"
+elif(( num == 5 ))
+then
+    echo "FIVE"
+elif(( num == 6 ))
+then
+    echo "SIX"
+elif(( num == 7 ))
+then
+    echo "SEVEN"
+elif(( num == 8 ))
+then
+    echo "EIGTH"
+elif(( num == 9 ))
+then
+    echo "NINE"
+else
+    echo "enter only one digit number"
+fi
+
